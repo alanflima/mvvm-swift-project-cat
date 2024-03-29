ea5e8ccf24d70821db24e13933668d9baae383c5
Alan Lima <alanflima@hotmail.com>
2024-03-28 21:39:12 -0300

    Commit inicial

---
+++ CatLove/CatLove/CatLoveApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatLoveApp.swift
+//  CatLove
+//
+//  Created by Alan Lima on 28/03/24.
+//
+
+import SwiftUI
+
+@main
+struct CatLoveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
